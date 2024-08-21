acd207b1deb375d6e4be585cb9fd26f525d6fde3
Sabrina Clementino <sabrinasantos783@outlook.com>
2024-08-20 23:21:44 -0300

    [00 feat(index.ts): Manipulando elementos através do DOM]

---
+++ ts/index.ts
@@ -0,0 +1,4 @@
+const form = document.querySelector("#search-form > form");
+const input: HTMLInputElement | null =
+  document.querySelector("#input-localizacao");
+const sectionTempoInfo = document.querySelector("tempo-info");
