0d6b82814b6745b42c33ccf2c8bfc89f5b10ed18
Sabrina Clementino <sabrinasantos783@outlook.com>
2024-08-26 22:21:36 -0300

    [01] feat(index.ts) Adicionando evento ao formulário

--- ts/index.ts
+++ ts/index.ts
@@ -2,3 +2,7 @@ const form = document.querySelector("#search-form > form");
 const input: HTMLInputElement | null =
   document.querySelector("#input-localizacao");
 const sectionTempoInfo = document.querySelector("tempo-info");
+
+form?.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
