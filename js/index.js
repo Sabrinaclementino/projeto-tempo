64c1dd25f27262a1d97830aa785c301f5c542735
Sabrina Clementino <sabrinasantos783@outlook.com>
2024-08-28 23:21:38 -0300

    [02]feat(index): acessar API com chave própria

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const form = document.querySelector("#search-form > form");
+const input = document.querySelector("#input-localizacao");
+const sectionTempoInfo = document.querySelector("tempo-info");
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", (event) => __awaiter(void 0, void 0, void 0, function* () {
+    event.preventDefault();
+    if (!input)
+        return;
+    const localization = input.value;
+    try {
+        const response = yield fetch(`https://api.openweathermap.org/data/2.5/weather?q=${localization}&appid=d434620ca1717b66a2f90dc08801cec2&lang=pt_br&units=metric`);
+        const datas = yield response.json();
+        console.log(datas);
+    }
+    catch (error) {
+        console.log("Deu um erro na obtenção da API", error);
+    }
+}));
--- ts/index.ts
+++ ts/index.ts
@@ -3,6 +3,21 @@ const input: HTMLInputElement | null =
   document.querySelector("#input-localizacao");
 const sectionTempoInfo = document.querySelector("tempo-info");
 
-form?.addEventListener("submit", (event) => {
+form?.addEventListener("submit", async (event) => {
   event.preventDefault();
+
+  if (!input) return;
+
+  const localization = input.value;
+
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${localization}&appid=d434620ca1717b66a2f90dc08801cec2&lang=pt_br&units=metric`
+    );
+
+    const datas = await response.json();
+    console.log(datas);
+  } catch (error) {
+    console.log("Deu um erro na obtenção da API", error);
+  }
 });
