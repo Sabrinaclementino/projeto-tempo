ec1a376c413e59ba429cf8f7afcee7ab50bb9596
Sabrina Clementino <sabrinasantos783@outlook.com>
2024-09-03 14:43:05 -0300

    [03] feat(index): Manipular dados da API

--- ts/index.ts
+++ ts/index.ts
@@ -10,6 +10,11 @@ form?.addEventListener("submit", async (event) => {
 
   const localization = input.value;
 
+  if (localization.length < 3) {
+    alert("O local precisa ter, pelo menos, 3 letras");
+    return;
+  }
+
   try {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${localization}&appid=d434620ca1717b66a2f90dc08801cec2&lang=pt_br&units=metric`
@@ -17,6 +22,12 @@ form?.addEventListener("submit", async (event) => {
 
     const datas = await response.json();
     console.log(datas);
+
+    const infos = {
+      temperatura: Math.round(datas.main.temp),
+      local: datas.name,
+      icone: `https://api.openweathermap.org/img/wn/${datas.weather[0].icon}@2x.png`,
+    };
   } catch (error) {
     console.log("Deu um erro na obtenção da API", error);
   }
